c31d8590130f3a7a00f8097aada31c1213997eeb
AyushSantri <ayushsantri222@gmail.com>
2021-10-24 23:35:01 +0530

    created a function to reduce the repeated code and also created 7 buttons to produce 7 different sound

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,89 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  AudioPlayer audioPlayer = AudioPlayer();
+  void playSound() {
+    final player = AudioCache();
+    player.play('note1.wav');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-                print('clicked');
-              },
-              child: const Text(
-                'click me',
-                style: TextStyle(color: Colors.black),
+          child: Column(
+            children: [
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playSound();
+                },
+                child: const Text(
+                  'click me 1',
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
-            ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playSound();
+                },
+                child: const Text(
+                  'click me 2',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playSound();
+                },
+                child: const Text(
+                  'click me 3',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playSound();
+                },
+                child: const Text(
+                  'click me 4',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  playSound();
+                },
+                child: const Text(
+                  'click me 5',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playSound();
+                },
+                child: const Text(
+                  'click me 6',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  playSound();
+                },
+                child: const Text(
+                  'click me 7',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
           ),
         ),
       ),
